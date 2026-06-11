4ec61309c4651611a0d8621e26f4137efadef0ed
David Ramos Perdomo <dramos97.dr24@gmail.com>
2026-06-11 16:34:15 +0200

    web view enable

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,11 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.trackingfy.app',
   appName: 'Trackingfy',
-  webDir: 'dist/trackingfy/browser'
+  webDir: 'dist/trackingfy/browser',
+  server: {
+    url: 'https://trackingfy.web.app',
+    allowNavigation: ['trackingfy.web.app']
+  }
 };
 
 export default config;
