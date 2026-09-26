d834a72df341e3a4cdca829a47917bc400786c0f
David Ramos Perdomo <dramos97.dr24@gmail.com>
2026-09-26 11:41:28 +0200

    chore: release v1.0.6

--- src/app/services/translation.ts
+++ src/app/services/translation.ts
@@ -7,7 +7,7 @@ export type Lang = 'en' | 'es';
 })
 export class TranslationService {
   currentLang = signal<Lang>('es');
-  version = '1.0.5';
+  version = '1.0.6';
 
   private translations: Record<Lang, Record<string, string>> = {
     es: {
